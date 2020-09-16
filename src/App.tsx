f5214b162812fa0013712172bb80c147d7c7da65
James Vickery <dev@jamesvickery.net>
2020-09-16 23:47:30 +0100

    c o n t e n t

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,10 @@ import './App.scss'
 function App() {
   return (
     <div className="page">
+      <div className="top-bar-thing"></div>
+
+      <h1 style={{ margin: '2rem 0' }}>GH Pixelz</h1>
+
       <Editor />
     </div>
   )
